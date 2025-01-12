ace839cb790233ed01b0bc844c643fc5068a2157
Loesthard <bidbidouwtf@gmail.com>
2025-01-12 18:46:23 +0100

    Sécurité.php

--- register.php
+++ register.php
@@ -38,28 +38,39 @@ try {
 ?>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pseudo = $_POST['pseudo'];
-    $email = $_POST['email'];
-    $mdp = $_POST['password'];
-    $ecurie = $_POST['ecurie'];
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $mdp = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $ecurie = htmlspecialchars($_POST['ecurie']);
 
     $host = 'localhost';
     $dbname = 'e2202522';
     $user = 'e2202522';
     $password = 'metcquetuveux';
     
-    $login = new mysqli($_serveur,$utilisateur,$modepasse,$base);
+    $login = new mysqli($host,$user,$password,$dbname);
     if ($login->connect_error) die ("<p>Problème de co chef : " . $login->connect_error . "</p>");
 
-    if ($email && $pseudo && $password){
-        $stmt = $login->prepare("INSERT INTO users (pseudo, email, password, ecurie) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ss", $pseudo, $email, $mdp, $ecurie);
-        if ($stmt->execute()){
-            header("Location: connection.html");
-            exit;
+    if ($email && $pseudo && !empty($_POST(['password'])){
+        $emailexist = $login->prepare("SELECT email FROM users WHERE email = ?");
+        $emailexist->bind_param("s",$email);
+        $emailexist->execute();
+        $emailexist->store_result();
+
+        if ($emailexist->num_rows > 0) echo "<p>Erreur : rentre un bon email chef !</p>";
+
+        else {
+            $stmt = $login->prepare("INSERT INTO users (pseudo, email, password, ecurie) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $pseudo, $email, $mdp, $ecurie);
+            if ($stmt->execute()){
+                header("Location: connection.html");
+                exit;
+            }
+            else echo "<p>Erreur de merde : " . $stmt->error . "</p>";
+            $stmt->close();
         }
-        else if ($login->errno == 1062) echo "<p>Erreur : Cet email est déjà enregistré</p>";
-        else echo "<p>Erreur de merde : " . $stmt->error . "</p>";
+        $emailexist->close();
     }
+    $login->close();
 }
 ?>
