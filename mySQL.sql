5298d2f2d22760d697da8b433b6394c6d790edd3
Loesthard <bidbidouwtf@gmail.com>
2025-01-15 10:32:42 +0100

    création tableau des temps

--- mySQL.sql
+++ mySQL.sql
@@ -9,5 +9,9 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     ecurie VARCHAR(50),
+    monaco FLOAT,
+    shanghai FLOAT,
+    hockenheim FLOAT,
+    nuerburgring FLOAT,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
