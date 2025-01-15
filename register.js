7969b6707ddd40c8800cde0fda09e59b3f6e24ea
Loesthard <bidbidouwtf@gmail.com>
2025-01-15 10:22:16 +0100

    Create register.js

---
+++ register.js
@@ -0,0 +1,17 @@
+function verification(data){
+  const {pseudo, email, mdp} = data;
+
+  if (email && !/^\S+@\S+\.\S+$/.test(email)){
+    alert("envoie l'email chef");
+    return false;
+  }
+  if (mdp && mdp.length < 6){
+    alert("rentre un mot de passe plus fort chef 6 caractères");
+    return false;
+  }
+  if (username && username.trim() === ""){
+    alert("panne de pseudo chef");
+    return false;
+  }
+  return true;
+}
