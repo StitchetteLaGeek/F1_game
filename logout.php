b5fd23c779dc1177c163b29962159ef20c4075f9
Loesthard <bidbidouwtf@gmail.com>
2025-01-18 19:34:11 +0100

    destruction cookies et variables

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+setcookie("pseudo", "", time() - 3600, "/");
+setcookie("email","",time() - 3600, "/");
+session_unset();
 session_destroy();
 header("Location: index.php")
 exit();
