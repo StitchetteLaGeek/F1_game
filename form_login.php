c7926f9505f070d4bd0e120023ef2a1d740b455f
Loesthard <bidbidouwtf@gmail.com>
2025-01-19 15:43:07 +0100

    affichage des erreurs

--- form_login.php
+++ form_login.php
@@ -15,15 +15,27 @@ session_start();
         <h2>Connexion</h2>
         <form action="login.php" method="POST">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required><br>
+            <input type="email" id="email" name="email" required>
+            <?php
+                if (!empty($error['email'])) echo "<p id = 'error'>" . $error['email'] . "</p>";
+            ?>
+            <br>
         
             <label for="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required><br>
+            <input type="password" id="password" name="password" required>
+            <?php
+                if (!empty($error['mdp'])) echo "<p id = 'error'>" . $error['mdp'] . "</p>";
+            ?>
+            <br>
             <label> 
                 <input type="checkbox" name="remember"> Se souvenir de moi
             </label><br>
             <button type="submit">Se connecter</button>
         </form>
+
+        <?php
+            if (!empty($error['general'])) echo "<p id = 'error'>" . $error['general'] . "</p>";
+        ?>
 
         <p>Pas encore inscrit ? <a href="register.html">S'inscrire ici</a></p>
         <p>Retour à <a href="index.html">la page d'accueil</a></p>
