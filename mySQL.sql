0f61a1d9af02af20244a065ec76b55ac693f8c8c
Loesthard <bidbidouwtf@gmail.com>
2025-01-19 17:23:56 +0100

    push

--- mySQL.sql
+++ mySQL.sql
@@ -1,15 +1,21 @@
--- creation mysql
 -- Création de la base de données
 CREATE DATABASE IF NOT EXISTS course_game;
 USE course_game;
 
--- Création de la table des utilisateurs 
+-- Création de la table des utilisateurs
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     pseudo VARCHAR(50) NOT NULL,
-    age INT NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     ecurie VARCHAR(50),
+    monaco FLOAT DEFAULT NULL,
+    shanghai FLOAT DEFAULT NULL,
+    hockenheim FLOAT DEFAULT NULL,
+    nuerburgring FLOAT DEFAULT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    CONSTRAINT chk_email CHECK (email LIKE '%@%.%')
 );
+
+CREATE INDEX idx_email ON users(email);
